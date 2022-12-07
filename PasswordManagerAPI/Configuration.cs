f33e47670abfb6aa743856ecba88e8ee65c8566e
Ryan Gabriel <ryan.bromati@hotmail.com>
2022-12-07 13:02:24 -0300

    feat: token configuration

---
+++ PasswordManagerAPI/Configuration.cs
@@ -0,0 +1,7 @@
+namespace PasswordManagerAPI;
+
+public static class Configuration
+{
+   /* Json Web Token */
+   public static string JwtKey { get; set; } = "KQLSyYLqjCbr9fCHWvmZcLsQFqpibF=";
+}
---
+++ PasswordManagerAPI/Services/TokenService.cs
@@ -0,0 +1,30 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace PasswordManagerAPI.Services;
+
+/* classe que gera o token */
+public class TokenService
+{
+    public string GenerateToken(User user)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        /* criando variavel com a key em bytes */
+        var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+        /* configurando especificação do token */
+        var tokenDescriptor = new SecurityTokenDescriptor(
+        {
+            /* expiração do token */
+            Expires = DateTime.UtcNow.AddHours(8),
+            
+            /* assinatura do token */
+            SigningCredentials = new SigningCredentials(
+                /* key e método de encriptação */
+                new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        });
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
