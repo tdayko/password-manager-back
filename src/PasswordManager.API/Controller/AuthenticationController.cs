4555bb805f62a0d49bbe0f45a32dead619a9db90
Ryan Gabriel <ryan.bromati@hotmail.com>
2024-01-23 20:10:53 -0300

    fix: conflict between mediatr versions

--- src/PasswordManager.API/Controller/AuthenticationController.cs
+++ src/PasswordManager.API/Controller/AuthenticationController.cs
@@ -1,5 +1,7 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+
+using PasswordManager.Application.Authentication.RegisterCommand;
 using PasswordManager.Application.Contracts;
 
 namespace PasswordManager.API.Controller;
@@ -9,4 +11,13 @@ namespace PasswordManager.API.Controller;
 public class AuthenticationController(ISender sender) : ControllerBase
 {
     private readonly ISender _sender = sender;
+
+    [HttpPut]
+    public async Task<IActionResult> Register(RegisterRequest request)
+    {
+        var command = new RegisterCommand(request.Username, request.Email, request.Password);
+        var authResult = await _sender.Send(command);
+
+        return Ok(new AuthenticationResponse(Guid.NewGuid(), request.Username, request.Email, "token"));
+    }
 }
--- src/PasswordManager.API/Program.cs
+++ src/PasswordManager.API/Program.cs
@@ -4,6 +4,7 @@ using PasswordManager.IoC;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
 builder.Services.AddSwaggerGen(x =>
     x.SwaggerDoc("v1",
         new OpenApiInfo { Title = "Password Manager v2", Description = "Easily protect sensitive passwords with this API", Version = "v2" }
