f35742fd2e9d7d7f8922afce4224dfe6e0272877
Ryan Gabriel <ryan.bromati@hotmail.com>
2024-02-12 19:58:57 -0300

    feat: add validation attributes to loginrequest and registerrequest classes

--- src/PasswordManager.Application/Authentication/Contracts/LoginRequest.cs
+++ src/PasswordManager.Application/Authentication/Contracts/LoginRequest.cs
@@ -1,3 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PasswordManager.Application.Authentication.Contracts;
 
-public record LoginRequest(string Email, string Password);
+public record LoginRequest
+{
+    [Required]
+    [EmailAddress]
+    public required string Email { get; set; }
+    [Required]
+    public required string Password { get; set; }
+}
--- src/PasswordManager.Application/Authentication/Contracts/RegisterRequest.cs
+++ src/PasswordManager.Application/Authentication/Contracts/RegisterRequest.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PasswordManager.Application.Authentication.Contracts;
 
 public record RegisterRequest
 {
+    [Required]
     public required string Username { get; set; }
+    [EmailAddress]
+    [Required]
     public required string Email { get; set; }
+    [Required]
     public required string Password { get; set; }
 }
