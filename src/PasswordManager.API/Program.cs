275a529e1fdddf273a16eaec16bc5df57fbf3d68
Ryan Gabriel <ryan.bromati@hotmail.com>
2024-02-12 23:12:45 -0300

    refactor: program.cs file

--- src/PasswordManager.API/Program.cs
+++ src/PasswordManager.API/Program.cs
@@ -3,26 +3,18 @@ using PasswordManager.IoC;
 using PasswordManager.API.Extensions;
 using PasswordManager.API.EndPoints;
 
-#region Builder Services
-
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddMediatR();
 builder.Services.AddIoC(builder.Configuration);
 builder.Services.AddCustomSwagger();
 
-#endregion
 WebApplication app = builder.Build();
 
-#region App Settings
-
 app.UseHttpsRedirection();
-
 app.AddAuthenticationEndpoint();
 app.AddCustomErrors();
 app.AddCustomSwagger();
 
-#endregion
 app.Run();
