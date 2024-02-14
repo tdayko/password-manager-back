2fc74cca5da5140e1cc7d0f8964acefadf43c9ca
Ryan Gabriel <ryan.bromati@hotmail.com>
2024-02-13 23:59:14 -0300

    feat: description attributes to LoginRequest and RegisterRequest properties

--- src/PasswordManager.Application/Authentication/Contracts/LoginRequest.cs
+++ src/PasswordManager.Application/Authentication/Contracts/LoginRequest.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace PasswordManager.Application.Authentication.Contracts;
@@ -6,7 +7,9 @@ public record LoginRequest
 {
     [Required]
     [EmailAddress]
+    [Description("The email of the user")]
     public required string Email { get; set; }
     [Required]
+    [Description("The password of the user")]
     public required string Password { get; set; }
 }
--- src/PasswordManager.Application/Authentication/Contracts/RegisterRequest.cs
+++ src/PasswordManager.Application/Authentication/Contracts/RegisterRequest.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace PasswordManager.Application.Authentication.Contracts;
@@ -5,10 +6,13 @@ namespace PasswordManager.Application.Authentication.Contracts;
 public record RegisterRequest
 {
     [Required]
+    [Description("The username of the user")]
     public required string Username { get; set; }
     [EmailAddress]
     [Required]
+    [Description("The email of the user")]
     public required string Email { get; set; }
     [Required]
+    [Description("The password of the user")]
     public required string Password { get; set; }
 }
