f96ae8bcf55d214bb9e3f3371bd06db19693ab36
urltapas <ryan.bromati@hotmail.com>
2024-03-31 20:28:48 -0300

    #

--- src/PasswordManager.API/Program.cs
+++ src/PasswordManager.API/Program.cs
@@ -2,10 +2,6 @@ using PasswordManager.API.Endpoints;
 using PasswordManager.API.Extensions;
 using PasswordManager.Application;
 using PasswordManager.IoC;
-using DotNetEnv;
-using DotNetEnv.Configuration;
-using PasswordManager.API.Helpers;
-
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services
@@ -13,11 +9,6 @@ builder.Services
     .AddMediatR()
     .AddIoC(builder.Configuration)
     .AddCustomSwagger();
-
-string currentDir = Directory.GetCurrentDirectory();
-builder.Configuration
-    .AddDotNetEnv(EnvFileManager.GetEnvFilePath(currentDir), LoadOptions.TraversePath())
-    .Build();
 
 WebApplication app = builder.Build();
 
