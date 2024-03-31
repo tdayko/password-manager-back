64813482af45268810114d98c08be5c22845721d
urltapas <ryan.bromati@hotmail.com>
2024-03-31 17:14:42 -0300

    chore: add .env

--- src/PasswordManager.API/Program.cs
+++ src/PasswordManager.API/Program.cs
@@ -2,8 +2,15 @@ using PasswordManager.API.Endpoints;
 using PasswordManager.API.Extensions;
 using PasswordManager.Application;
 using PasswordManager.IoC;
+using DotNetEnv;
+using DotNetEnv.Configuration;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+string rootPath = Directory.GetCurrentDirectory();
+builder.Configuration
+    .AddDotNetEnv(Path.Combine(Directory.GetParent(rootPath)!.Parent!.FullName, ".env"), LoadOptions.TraversePath())
+    .Build();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddMediatR();
@@ -13,7 +20,6 @@ builder.Services.AddCustomSwagger();
 WebApplication app = builder.Build();
 
 app.UseHttpsRedirection();
-
 app.AddAuthenticationEndpoint();
 app.AddCustomErrors();
 app.AddCustomSwagger();
