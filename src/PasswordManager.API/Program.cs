bf0500481164dde7dfc81c415fd9956302643a1c
urltapas <ryan.bromati@hotmail.com>
2024-03-31 19:46:17 -0300

    feat: add envfilemanager

--- src/PasswordManager.API/Program.cs
+++ src/PasswordManager.API/Program.cs
@@ -4,19 +4,20 @@ using PasswordManager.Application;
 using PasswordManager.IoC;
 using DotNetEnv;
 using DotNetEnv.Configuration;
+using PasswordManager.API.Helpers;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-string envFilePath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory())!.Parent!.FullName, ".env");
-builder.Configuration
-    .AddDotNetEnv(envFilePath, LoadOptions.TraversePath())
-    .Build();
 
 builder.Services
     .AddEndpointsApiExplorer()
     .AddMediatR()
     .AddIoC(builder.Configuration)
     .AddCustomSwagger();
+
+string currentDir = Directory.GetCurrentDirectory();
+builder.Configuration
+    .AddDotNetEnv(EnvFileManager.GetEnvFilePath(currentDir), LoadOptions.TraversePath())
+    .Build();
 
 WebApplication app = builder.Build();
 
