ac293a76fc9ecf0f4abc8c7a5352fd1c8e94e1c3
urltapas <ryan.bromati@hotmail.com>
2024-04-02 21:20:16 -0300

    #

--- src/PasswordManager.Application/Persistence/IUserRepository.cs
+++
@@ -1,8 +0,0 @@
-using PasswordManager.Domain.Entities;
-namespace PasswordManager.Application.Persistence;
-
-public interface IUserRepository
-{
-    User? GetUserByEmail(string email);
-    void AddUser(User user);
-}
