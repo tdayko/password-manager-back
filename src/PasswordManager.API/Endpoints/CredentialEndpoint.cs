386a09b9d34c1823cdddb3fe3486f067e85139a8
urltapas <ryan.bromati@hotmail.com>
2024-04-13 21:21:18 -0300

    feat: add getusercredentials and getallusercreedential endpoints

--- src/PasswordManager.API/Endpoints/CredentialEndpoint.cs
+++ src/PasswordManager.API/Endpoints/CredentialEndpoint.cs
@@ -24,18 +24,68 @@ public static class CredentialEndpoint
                 x.Summary = "Add a new credential";
                 return x;
             });
+        
+        credentialEndpoint.MapGet("/{credentialId}", HandleGetUserCredentialById)
+            .WithName("Get User Credential by Id")
+            .Produces<StandardSuccessResponse<CredentialResponse>>()
+            .WithOpenApi(x =>
+            {
+                x.Summary = "Get User Credential by Id";
+                return x;
+            });
 
+        credentialEndpoint.MapGet("/", HandleGetAllUserCredentials)
+            .WithName("Get All User Credentials")
+            .Produces<StandardSuccessResponse<List<CredentialResponse>>>()
+            .WithOpenApi(x => 
+            {
+                x.Summary = "Get All User Credentials";
+                return x;
+            });
+        
         return app;
     }
 
     #region private methods
-    private static async Task<IResult> HandleAddCredential(
+
+    private static async Task<IResult> HandleGetAllUserCredentials(
         HttpContext context,
-        AddCredentialRequest request,
         ICredentialRepository credentialRepository,
-        IUserRepository userRepository,
         IMapper mapper)
     {
+        if (!context.User.Identity!.IsAuthenticated) throw new UnauthenticatedUserException();
+        var userId = Guid.Parse(context.User.FindFirstValue("name")!);
+
+        var credentials = credentialRepository.GetAllUserCredentials(userId);
+        if (credentials.Count == 0) throw new NoCredentialsWereFoundException();
+
+        var credentialsResponses = mapper.Map<List<CredentialResponse>>(credentials);
+        return Results.Ok(new StandardSuccessResponse<List<CredentialResponse>>(credentialsResponses));
+    }
+    private static async Task<IResult> HandleGetUserCredentialById(
+        string credentialId, 
+        HttpContext context, 
+        ICredentialRepository credentialRepository, 
+        IMapper mapper)
+    {
+        if (!context.User.Identity!.IsAuthenticated) throw new UnauthenticatedUserException();
+        
+        var userId = Guid.Parse(context.User.FindFirstValue("name")!);
+        var credential = credentialRepository.GetUserCredentialById(Guid.Parse(credentialId), userId);
+        
+        if (credential is null) throw new CredentialNotFoundException();
+        
+        var credentialResponse = mapper.Map<CredentialResponse>(credential);
+        return Results.Ok(new StandardSuccessResponse<CredentialResponse>(credentialResponse));
+    }
+    
+    private static async Task<IResult> HandleAddCredential(
+        AddCredentialRequest request, 
+        HttpContext context,
+        IUserRepository userRepository, IMapper mapper, 
+        ICredentialRepository credentialRepository
+        )
+    { 
         if (!context.User.Identity!.IsAuthenticated) throw new UnauthenticatedUserException();
         
         var user = userRepository!.GetUserByUserId(Guid.Parse(context.User.FindFirstValue("name")!))!;
@@ -45,5 +95,6 @@ public static class CredentialEndpoint
         var credentialResponse = mapper.Map<CredentialResponse>(credential);
         return Results.Ok(new StandardSuccessResponse<CredentialResponse>(credentialResponse));
     }
+    
     #endregion
 }
