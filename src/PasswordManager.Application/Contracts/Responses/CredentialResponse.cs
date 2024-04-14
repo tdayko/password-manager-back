4c0bfb5b98c44dfd1820a49b9c99e34fcf6d1c5f
urltapas <ryan.bromati@hotmail.com>
2024-04-13 21:20:12 -0300

    feat: add id to credential response

--- src/PasswordManager.Application/Contracts/Responses/CredentialResponse.cs
+++ src/PasswordManager.Application/Contracts/Responses/CredentialResponse.cs
@@ -2,11 +2,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PasswordManager.Application.Contracts.Responses;
 
-public class CredentialResponse(Guid userId, string username, string email, string password, string webSite)
+public class CredentialResponse(Guid id, Guid userId, string username, string email, string password, string webSite)
 {
-    public Guid UserId { get; private set; } = userId;
+    public Guid Id { get; private set; } = id;
     public string Username { get; private set; } = username;
     [EmailAddress] public string Email { get; private set; } = email;
     public string Password { get; private set; } = password;
     [Url] public string WebSite { get; private set; } = webSite;
+    public Guid UserId { get; private set; } = userId;
 }
