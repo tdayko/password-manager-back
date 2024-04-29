51fb5661f4907beefc96a2c83121b9a62258f1b4
urltapas <ryan.bromati@hotmail.com>
2024-04-28 22:42:03 -0300

    feat: add db context

---
+++ src/PasswordManager.Infra/DbContext/PasswordManagerContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using PasswordManager.Domain.Entities;
+
+namespace PasswordManager.Infra.DbContext;
+
+public class PasswordManagerContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    public DbSet<User>? Users { get; set; }
+    public DbSet<Credential>? Credentials { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseInMemoryDatabase(databaseName: "PasswordManagerDb");
+
+    }
+}
